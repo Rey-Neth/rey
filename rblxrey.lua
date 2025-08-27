d86c331e5b40710869b0649f1acd7008f4bfe786
Rey-Neth <superkatttt@gmail.com>
2025-08-27 16:02:33 +0300

    Update rblxrey.lua

--- rblxrey.lua
+++ rblxrey.lua
@@ -304,10 +304,10 @@ ScriptsTab:Button({
     Title = "Browser (Dex Explorer) Lite",
     Desc  = "Открыть браузер объектов",
     Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Rey-Neth/rey/refs/heads/main/dex%20lite"))()
+        loadstring(game:HttpGet("https://gist.githubusercontent.com/dannythehacker/1781582ab545302f2b34afc4ec53e811/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4"))()
     end
 })
-
-
+--https://gist.githubusercontent.com/dannythehacker/1781582ab545302f2b34afc4ec53e811/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4--
+--https://raw.githubusercontent.com/Rey-Neth/rey/refs/heads/main/dex%20lite--
 
 Window:SelectTab(1)
