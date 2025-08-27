80d0efbdd9cec70be4c8c79b70cc777681329c4a
Rey-Neth <superkatttt@gmail.com>
2025-08-27 15:11:09 +0300

    Update rblxrey.lua

--- rblxrey.lua
+++ rblxrey.lua
@@ -297,7 +297,7 @@ ScriptsTab:Button({
     Title = "Browser (Dex Explorer)",
     Desc  = "Открыть браузер объектов",
     Callback = function()
-        loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Dex%20Explorer.lua"))()
+        loadstring(game:HttpGet("https://gist.githubusercontent.com/dannythehacker/1781582ab545302f2b34afc4ec53e811/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4"))()
     end
 })
 
